c7a27c2af1d49cb5e6ce28cd137adc346f7eda65
sugarshin <shinsugar@gmail.com>
2015-07-04 18:37:35 +0900

    change babel/polyfill to babel-runtime

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,8 +1,10 @@
-import 'babel/polyfill';
 import React from 'react';
 import { run, HashLocation } from 'react-router';
+import regeneratorRuntime from 'babel-runtime/regenerator';
 
 import routes from './config/routes';
+
+global.regeneratorRuntime = regeneratorRuntime;
 
 run(routes, HashLocation, Root => {
   React.render(<Root />, document.getElementById('container'));
